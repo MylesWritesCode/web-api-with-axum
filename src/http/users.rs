5d3b177280b617abb0f343eac23afc8e4f44af76
Myles Berueda <myles@themapletree.io>
2022-05-22 15:38:12 -0700

    exp with query params

--- src/http/users.rs
+++ src/http/users.rs
@@ -1,9 +1,27 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Json, Router, extract::Path};
+use serde::{Deserialize, Serialize};
 
 pub fn router() -> Router {
-    return Router::new().route("/users", get(get_users));
+    return Router::new()
+        .route("/users", get(get_users))
+        .route("/users/:id", get(get_user));
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct User {
+    id: Option<i32>,
+    name: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct UserResponse {
+    user: User,
 }
 
 async fn get_users() -> &'static str {
     return "from get_users";
+}
+
+async fn get_user(Path(user_id): Path<String>) -> Json<UserResponse> {
+    todo!()
 }
